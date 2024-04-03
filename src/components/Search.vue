ba1f994bb0d260396904db463c3b6ce583154d86
vmwebio <vmwebio@gmail.com>
2024-04-03 12:49:45 +0300

    add component Search.vue

---
+++ src/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="">
+    <input
+      type="text"
+      class="search-input"
+      placeholder="Input movie"
+      v-model="searchMovie"
+    />
+  </form>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const searchMovie = ref("");
+</script>
+
+<style scoped>
+.search-input {
+  border: 1px solid #e7e7e7;
+  width: 100%;
+  height: 40px;
+  padding: 0 10px;
+  margin-bottom: 20px;
+  border-radius: 10px;
+}
+</style>
